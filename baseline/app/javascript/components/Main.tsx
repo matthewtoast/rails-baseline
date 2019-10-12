4c775cf5a06215f8b5afcbb4133c3c486a5b5d79
Matthew Trost <mrtrost@gmail.com>
2019-10-12 12:46:17 -0400

    Add missing import to component

--- baseline/app/javascript/components/Main.tsx
+++ baseline/app/javascript/components/Main.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {HomePage} from './pages/HomePage';
 
 export class Main extends React.Component<{}, {}> {
